5d09b6c5bdd5d3f0c249ac5cba23d0b80c98d255
Keyodinfire <63654115+Keyodinfire@users.noreply.github.com>
2021-03-25 14:10:40 -0400

    Add files via upload

---
+++ data/config.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql');
+const config = {
+    host: '162.243.235.211',
+    user: 'user345',
+    password: 'Password1@',
+    database: 'db345'
+};
+const pool = mysql.createPool(config);
+
+module.exports = pool;
